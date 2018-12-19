0ae99791d84921b1b6c64fbe84912a73958b218d
realDrCastafolte <alexandre.monties@gmail.com>
2018-12-19 22:30:20 +0100

    day4(wip): repose record

--- day4/src/guard.rs
+++ day4/src/guard.rs
@@ -5,33 +5,45 @@ use std::time::Duration;
 #[derive(Clone,Debug)]
 pub struct Guard {
     id: u32,
-    shift: Shift,
-    sleep: Duration
+    repose: Duration
 }
 
 impl Guard {
 
-    pub fn new(id: u32, shift: Shift) -> Guard {
-        Guard { id: id, shift: shift, sleep: Duration::new(0, 0) }
+    pub fn new(id: u32) -> Guard {
+        Guard { id: id, repose: Duration::new(0, 0) }
     }
 
-    pub fn sleep(&self, at: Instant) -> SleepingGuard {
-        SleepingGuard { guard: self.clone(), start: at }
+    pub fn take(&self, shift: Shift) -> OnShiftGuard {
+        OnShiftGuard { guard: self.clone(), shift: shift }
     }
 
 }
 
-pub struct SleepingGuard {
+#[derive(Clone,Debug)]
+pub struct OnShiftGuard {
     guard: Guard,
+    shift: Shift
+}
+
+impl OnShiftGuard {
+
+    pub fn sleep(&self, at: Instant) -> SleepingGuard {
+        SleepingGuard { on_shift: self.clone(), start: at }
+    }
+}
+
+pub struct SleepingGuard {
+    on_shift: OnShiftGuard,
     start: Instant
 }
 
 impl SleepingGuard {
 
-    pub fn wake_up(&self, at: Instant) -> Guard {
-        let mut awaken_guard = self.guard.clone();
-        awaken_guard.sleep += at.duration_since(self.start);
-        awaken_guard
+    pub fn wake_up(&self, at: Instant) -> OnShiftGuard {
+        let mut on_shift = self.on_shift.clone();
+        on_shift.guard.repose += at.duration_since(self.start);
+        on_shift
     }
 }
 
@@ -44,31 +56,40 @@ mod tests {
     use super::Guard;
 
     #[test]
-    fn should_have_no_sleeping_time() {
-        let guard = Guard::new(1, Shift::new(Instant::now()));
-        assert_eq!(guard.sleep, Duration::new(0, 0));
+    fn should_have_no_repose_before_taking_shift() {
+        assert_eq!(Guard::new(1).repose, Duration::new(0, 0));
     }
 
     #[test]
-    fn should_have_10_sec_sleeping_time() {
-        let sleep_instant = Instant::now();
-        let guard = Guard::new(1, Shift::new(Instant::now()))
-                        .sleep(sleep_instant)
-                        .wake_up(sleep_instant + Duration::new(10, 0));
-        assert_eq!(guard.sleep, Duration::new(10, 0));
+    fn should_have_no_repose_when_taking_shift() {
+        let on_shift = Guard::new(1).take(a_shift());
+        assert_eq!(on_shift.guard.repose, Duration::new(0, 0));
+    }
+
+    #[test]
+    fn should_have_10_sec_repose() {
+        let now = Instant::now();
+        let on_shift = Guard::new(1).take(a_shift())
+                        .sleep(now)
+                        .wake_up(now + seconds(10));
+        assert_eq!(on_shift.guard.repose, seconds(10));
     }
 
     #[test]
     fn should_have_30_sec_sleeping_time() {
-        let sleep_instant = Instant::now();
-        let guard = Guard::new(1, Shift::new(Instant::now()))
-                        .sleep(sleep_instant)
-                        .wake_up(sleep_instant + seconds(10))
-                        .sleep(sleep_instant + seconds(20))
-                        .wake_up(sleep_instant + seconds(30))
-                        .sleep(sleep_instant + seconds(40))
-                        .wake_up(sleep_instant + seconds(50));
-        assert_eq!(guard.sleep, seconds(30));
+        let now = Instant::now();
+        let on_shift = Guard::new(1).take(a_shift())
+                        .sleep(now)
+                        .wake_up(now + seconds(10))
+                        .sleep(now + seconds(20))
+                        .wake_up(now + seconds(30))
+                        .sleep(now + seconds(40))
+                        .wake_up(now + seconds(50));
+        assert_eq!(on_shift.guard.repose, seconds(30));
+    }
+
+    fn a_shift() -> Shift {
+        Shift::new(Instant::now())
     }
 
     fn seconds(seconds: u64) -> Duration {
